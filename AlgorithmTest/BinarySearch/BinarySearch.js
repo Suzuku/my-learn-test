3c26fe51af998b44d42608a732dafccf6ed4ba58
ganweilun <ganweilun@hikvision.com.cn>
2021-01-05 15:58:49 +0800

    DONE:完成二分查找算法

--- AlgorithmTest/BinarySearch/BinarySearch.js
+++ AlgorithmTest/BinarySearch/BinarySearch.js
@@ -1,15 +1,23 @@
 // Binary Search 二分查找 要求有序表  时间复杂度O(log2n)
-
-function BinarySearch(arr,target){
-    let midIndex=Math.floor(arr.length/2)
-    if(arr[midIndex]===target){
-        return midIndex
+let INDEX = -1;
+let end=false
+function BinarySearch(arr,index,target) {
+    // 跳出循环的条件
+    if(!end){
+        let midIndex = Math.floor(arr.length / 2);
+        if (arr[midIndex] === target) {
+            end=true
+            INDEX=index+midIndex
+            return end
+        }
+        console.log(midIndex, INDEX);
+        if (arr.length > 1) {
+            // 考虑传Index进递归 不然无法获取到基于总数组的index
+            BinarySearch(arr.slice(0, midIndex), index,target);
+            BinarySearch(arr.slice(midIndex, arr.length), index+midIndex,target);
+        }
     }
-    if(midIndex>0){
-        BinarySearch(arr.slice(0,midIndex),target)
-        BinarySearch(arr.slice(midIndex,arr.length),target)
-    }
-    return -1
+    return INDEX
 }
 
-console.log('result',BinarySearch([1,2,3,4,5,7,8,12],8)); 
+console.log('result', BinarySearch([1, 2, 3, 4, 5, 7, 8, 12],0,13));
