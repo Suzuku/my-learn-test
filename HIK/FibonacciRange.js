cc665ac44318aec378102fb309c6a69ab5214bac
Kiri <531008212@qq.com>
2021-02-03 23:23:53 +0800

    update:子序和加上注释

--- HIK/FibonacciRange.js
+++ HIK/FibonacciRange.js
@@ -1,9 +1,10 @@
-let arr=[]
-arr[0]=1
-arr[1]=1
+// 生成斐波那契数列
+let arr = []
+arr[0] = 1
+arr[1] = 1
 
-for(let i=2;i<10;i++){
-    arr[i]=arr[i-1]+arr[i-2]
+for (let i = 2; i < 10; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2]
 }
 
-console.log(arr);
+console.log(arr)
