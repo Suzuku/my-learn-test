c7290628bfe4939f3f92c4734ea2154c9b706859
Kiri <531008212@qq.com>
2021-02-18 12:18:08 +0800

    update:复习typescript 更新demo代码

--- HIK/typescript/genericClass.ts
+++ HIK/typescript/genericClass.ts
@@ -1,9 +1,37 @@
-class generic<T> {
-    arg:T;
-    add(x:T,y:T){
-        return T
-    };
+class Queue {
+    private data = []
+    push(item) {
+        return this.data.push(item)
+    }
+    pop() {
+        return this.data.shift()
+    }
 }
 
-let Generic=new generic<Number>()
-Generic.arg=4
+const queue = new Queue()
+queue.push(1)
+queue.push('str')
+console.log(queue.pop().toFixed())
+console.log(queue.pop().toFixed())
+
+//在上述代码中存在一个问题，它允许你向队列中添加任何类型的数据，当然，当数据被弹出队列时，也可以是任意类型。在上面的示例中，看起来人们可以向队列中添加string 类型的数据，但是那么在使用的过程中，就会出现我们无法捕捉到的错误，
+
+class Queue<T> {
+    private data = []
+    push(item: T) {
+        return this.data.push(item)
+    }
+    pop(): T {
+        return this.data.shift()
+    }
+}
+const queue = new Queue<number>()
+
+//泛型和 interface
+interface KeyPair<T, U> {
+    key: T
+    value: U
+}
+
+let kp1: KeyPair<number, string> = { key: 1, value: 'str' }
+let kp2: KeyPair<string, number> = { key: 'str', value: 123 }
