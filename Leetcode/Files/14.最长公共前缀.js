df1b47e55d635436b8c46971088474b3826619e3
Kiri <531008212@qq.com>
2021-03-08 00:09:05 +0800

    update:最长公共前缀

--- Leetcode/Files/14.最长公共前缀.js
+++ Leetcode/Files/14.最长公共前缀.js
@@ -9,14 +9,26 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-let arr=[]
-let result=[]
-for(let i=0;i<strs.length;i++){
-    arr[i]=strs[i].split('')
-}
-console.log(result)
-};
-longestCommonPrefix(["flower","flow","flight"])
-// @lc code=end
+var longestCommonPrefix = function (strs) {
+    let result = []
+    let successNumArr = []
+    for (let i = 0; i < strs.length - 1; i++) {
+        console.log(strs[i], strs[i + 1])
 
+        for (let j = 0; j < strs[i].length; j++) {
+            successNumArr[j] = 0
+            if (strs[i][j] === strs[i + 1][j]) {
+                successNumArr[j]++
+            }
+        }
+
+        // if (successNumArr === strs.length - 1) {
+        //     result.push(strs[i][0])
+        // }
+        console.log(successNumArr)
+    }
+
+    console.log(result)
+}
+longestCommonPrefix(['flower', 'flow', 'flight'])
+// @lc code=end
