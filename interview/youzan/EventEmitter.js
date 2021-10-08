ad4cc84011a36192c7d134e22d7001d403a6dad4
ganweilun <ganweilun@youzan.com>
2021-10-08 20:42:48 +0800

    feat: 事件模型实例

---
+++ interview/youzan/EventEmitter.js
@@ -0,0 +1,18 @@
+// 题目 2
+
+/**
+ * 实现一个事件模型（EventEmitter）
+ * 包括简单的 `on`、`emit`、`off`、`once` 等功能
+ */
+
+
+
+class EventEmitter {
+    constructor(){}
+
+}
+
+const event = new EventEmitter()
+
+// 示例写法
+event.on()
